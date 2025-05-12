6b0cc1893ec4bfa3b7cc12c8de794dff85025a80
The Vrapi <101073476+thevrapi@users.noreply.github.com>
2025-05-12 14:31:54 +0200

    javascript intro

---
+++ javascript/index.js
@@ -0,0 +1,58 @@
+// PRIMITIVE DATA TYPES
+// Boolean
+true
+false
+// String/Text
+"Relando Vrapi"
+//Numbers
+const year = 1996
+
+12.98
+1_000_000
+//Empty Values
+null
+undefined
+//BigInt
+let amount = 2_000_000_000_000n
+100n
+
+//Symbols
+Symbol("Relando")
+
+// COMPLEX DATA TYPES
+//Objects
+// {
+//     name: "Relando Vrapi",
+//     age: 28,
+//     isMarried: false,
+//     hobbies: ["hiking", "swimming", "play guitar"],
+//     address: {
+//         street: "street 234",
+//         city: "tirana",
+//         zip: 10017,
+//         country: "Albania",
+//     }
+// }
+
+// {
+//     name: "John Smith",
+//     age: 44,
+//     isMarried: true,
+//     hobbies: ["hiking", "swimming", "play guitar"],
+//     address: {
+//         street: "street 234",
+//         city: "tirana",
+//         zip: 10017,
+//         country: "Albania",
+//     }
+// }
+// Arrays/Lists
+// ["bannana", "apple", "cherry", "pineapple", "bluebarry"]
+// [23, 33, 1, 9, 124, 96]
+// [[1, 3, 5], [44,67,89,99], [1234,5467,8767,3765]]
+
+
+//Functions
+function sum() {
+
+}
---
+++ javascript/variables.js
@@ -0,0 +1,12 @@
+// Variables can be declared with let & const
+
+let fullname = "Jack Smith" //1. Declaring a variable/ 2. Define/Initialize a variable
+console.log(fullname)
+
+fullname = "John Doe" //3. Redefine/Re-asign a variable
+
+fullname = "Tinna Joe" //3. Redefine/Re-asign a variable
+
+const birthdate = 1996 //a. This is a constant variable, its value cannot be changed after assignment
+
+fullname = birthdate
