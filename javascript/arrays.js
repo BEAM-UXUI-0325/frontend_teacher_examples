bb97eff4d447e24fedd5fea03ce8a62bfbdef039
The Vrapi <101073476+thevrapi@users.noreply.github.com>
2025-05-19 12:06:25 +0200

    working with arrays

--- javascript/arrays.js
+++ javascript/arrays.js
@@ -7,7 +7,17 @@ let months = [1,2,3,4,5,6,7,8,9,10,11,12] //Length: 12
 let misc = ["hello", 12, true, null, undefined, NaN, Symbol(), 300n, [], {}, function abc(){},,]
 
 
-//Doing CRUD with the array: Create, Read, Update, Delete
-console.log(days.length)
+//Doing CRUD with the array: Create, Read, Update, Delete----------------------")
 
-console.log(days[days.length - 1])
+// days[0] = "MON"
+// days.push("RELANDO")
+// days.unshift("JACK")
+// days.pop()
+// days.shift()
+
+days[1] // read value
+days.push("some item") //add value at the end
+days.unshift("add at the begg..") //add value at the start
+days[0] = "updated content" //update a value
+days.pop() //delete value at the end
+days.shift() //delete value at the start
