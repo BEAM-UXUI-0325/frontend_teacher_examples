6d5b9ebe8bfce9279d39dc6ccb2c56880b1f8a61
The Vrapi <101073476+thevrapi@users.noreply.github.com>
2025-05-19 15:08:30 +0200

    students auditorium excerice

---
+++ javascript/students_array_exc.js
@@ -0,0 +1,9 @@
+// you have three classes with students that do lessions together and simars in separate classes
+// now merge the classes together and order them alphabetically and then choose 5 students at the start of the list
+// and display them
+// (if you want to add difficulty: choose 5 students at the end instead)
+// (to make it even more harder, choose 5 students in the middle of the class)
+
+// const class1 = ["Jack", "Tinna", "Lola"]
+// const class2 = ["Jack", "Tinna", "Lola"]
+// const class3 = ["Jack", "Tinna", "Lola"]
