42d1feda27f519e53c0b36cdf1e55a1ad7a5c953
The Vrapi <101073476+thevrapi@users.noreply.github.com>
2025-05-20 10:22:20 +0200

    Loops added

---
+++ javascript/Loops_Lession/loops.js
@@ -0,0 +1,37 @@
+
+// for(let a = 0; a < 10; a++){
+//     console.log("Hi")
+// }
+
+// let x = 0
+// while(x < 0){
+//     console.log(x + " Bye")
+//     x++
+// }
+
+// let y = 0
+// do {
+//     console.log(y + " Hello")
+//     y++
+// }
+// while(y < 0)
+
+const cars = ["Audi", "WV", "BMW", "Fiat", "Benz", "Peugeot", "Citroen"] 
+// for(let i = 0; i < cars.length; i++){
+//     console.log(cars[i])
+// }
+for(let car of cars){
+    console.log(car)
+}
+
+
+const hobbies = ["driving", "riding", "hiking", "sailing"]
+for(let hobby of hobbies){
+    console.log(hobby)
+}
+
+
+const students = ["Marco", "Nur", "Albert", "Saylee", "Amir"]
+for(let student of students){
+    console.log(student)
+}
